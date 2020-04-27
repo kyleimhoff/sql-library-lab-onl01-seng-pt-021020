4ff95a89fa26f2386664fb479ac2ba9a4f7f6d39
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-04-27 18:13:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,4 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "THE HOBBIT", 1, 1), (2, "Harry Potter", 2, 2);
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Lord of the Rings", 1, 1), (2, "Harry Potter", 2, 2);
 INSERT INTO subgenres (id, name) VALUES (1, "Fantacy"), (2, "Magic");
 INSERT INTO authors (id, name) VALUES (1, "Tolkein"), (2, "Rowling");
+INSERT INTO books (id, title, year, series_id) VALUES (1, "")
