2899eb08547763324eecef0952def08b7627fdfd
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-04-27 18:23:38 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Lord of the Rings", 1, 1), (2, "Harry Potter", 2, 2);
 INSERT INTO subgenres (id, name) VALUES (1, "Fantacy"), (2, "Magic");
 INSERT INTO authors (id, name) VALUES (1, "Tolkein"), (2, "Rowling");
-INSERT INTO books (id, title, year, series_id) VALUES (1, "The Fellowship of the Ring", 1954, 1), (2, "The Two Towers", 1954, 1), (3, "The Return of the King", 1955, 1), (4, "The Sorcerers Stone", 1997, 2), (5, "The Chamber of Secrets", 1998, 2), (6, "The Prisoner of Azkaban", 1999, 2), (7, "The Goblet of Fire", 2000, 2), (8, "The Order of the Pheonix", 2003, 2), (9, "The Half Blood Prince", 2005, 2), (10, "The Deathly Hallows", )
+INSERT INTO books (id, title, year, series_id) VALUES (1, "The Fellowship of the Ring", 1954, 1), (2, "The Two Towers", 1954, 1), (3, "The Return of the King", 1955, 1), (4, "The Sorcerers Stone", 1997, 2), (5, "The Chamber of Secrets", 1998, 2), (6, "The Prisoner of Azkaban", 1999, 2), (7, "The Goblet of Fire", 2000, 2), (8, "The Order of the Pheonix", 2003, 2), (9, "The Half Blood Prince", 2005, 2), (10, "The Deathly Hallows", 2007, 2);
